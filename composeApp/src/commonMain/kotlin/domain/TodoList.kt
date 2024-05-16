bd5c3bcf200ec42b0c3984c52b8f070c87f85b2a
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-05-16 21:24:46 +0530

    Realm object created

---
+++ composeApp/src/commonMain/kotlin/domain/TodoList.kt
@@ -0,0 +1,14 @@
+package domain
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class TodoTask :RealmObject {
+    @PrimaryKey
+    val _id:ObjectId = ObjectId()
+    var title = String()
+    var description = String()
+    var favorite:Boolean = false
+    var complete:Boolean = false
+}
